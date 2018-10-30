e3693faeb12fcec9dd29a5e89ef93d5a464f25d2
moonsung <visitant5@nate.com>
2018-10-30 17:16:21 +0900

    update
    update

---
+++ WinterCoding/src/main/java/com/example/ms/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.ms.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	   public String home() {
+		   System.out.println("==home==");
+		   return "home"; //home jsp 이동 
+	   }
+	
+}
