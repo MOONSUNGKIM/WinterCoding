6c26604eb6f4a7b7bffd5943a0298d6f92c944fe
moonsung <visitant5@nate.com>
2018-10-30 16:02:51 +0900

    update

---
+++ WinterCoding/src/main/java/com/example/ms/WinterCodingApplication.java
@@ -0,0 +1,12 @@
+package com.example.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterCodingApplication.class, args);
+	}
+}
